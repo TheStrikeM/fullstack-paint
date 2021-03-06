946ab21ad4bbc93a023c2e6a995265d55f5b06ce
Danil <dinkodanil@gmail.com>
2021-03-07 02:09:38 +0300

    Create all buttons

--- client/src/components/ToolBar.jsx
+++ client/src/components/ToolBar.jsx
@@ -5,6 +5,13 @@ const ToolBar = () => {
     return (
         <div className={"tool-bar"}>
             <button className={"tool-bar__btn brush"} />
+            <button className={"tool-bar__btn circle"} />
+            <button className={"tool-bar__btn eraser"} />
+            <button className={"tool-bar__btn line"} />
+            <button className={"tool-bar__btn rect"} />
+            <button className={"tool-bar__btn redo"} />
+            <button className={"tool-bar__btn save"} />
+            <button className={"tool-bar__btn undo"} />
         </div>
     );
 };
