4e0c35a0dda2ba5461aae07b3a3cdde857fe4d54
Danil <dinkodanil@gmail.com>
2021-03-07 03:20:42 +0300

    Add input control width in SettingsBar Component

--- client/src/components/SettingsBar.jsx
+++ client/src/components/SettingsBar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import '../styles/bar.sass'
+import toolState from "../store/toolState";
 
 const SettingsBar = () => {
     return (
         <div className={"setting-bar"}>
-            SettingsBar
+            <label htmlFor="line-width">Толщина линии: </label>
+            <input
+                onChange={e => toolState.setLineWidth(e.target.value)}
+                style={{margin: '0 10px'}}
+                id={"line-width"}
+                defaultValue={1}
+                type="number"
+                min={1}
+                max={50}
+            />
+
+
         </div>
     );
 };
